fc688a595753d15afdb056a0bda1ab776045a31f
Daniel Köhn <daniel.koehn@gmx.net>
2015-04-10 03:23:42 +0200

    Spatial 10th/12th order visco-elastic FD operators

--- src/update_s_visc_PML.c
+++ src/update_s_visc_PML.c
@@ -535,101 +535,276 @@ void update_s_visc_PML(int nx1, int nx2, int ny1, int ny2,
    }}
 		break;
 
-/*	case 10:
+	case 10:
 		for (j=ny1;j<=ny2;j++){
 			for (i=nx1;i<=nx2;i++){
-				vxx = (  hc[1]*(vx[j][i]  -vx[j][i-1])
+			
+			vxx = (  hc[1]*(vx[j][i]  -vx[j][i-1])
 				       + hc[2]*(vx[j][i+1]-vx[j][i-2])
 				       + hc[3]*(vx[j][i+2]-vx[j][i-3])
 				       + hc[4]*(vx[j][i+3]-vx[j][i-4])
-				       + hc[5]*(vx[j][i+4]-vx[j][i-5])
-				      )*dhi;
-				vyy = (  hc[1]*(vy[j][i]  -vy[j-1][i])
-				       + hc[2]*(vy[j+1][i]-vy[j-2][i])
-				       + hc[3]*(vy[j+2][i]-vy[j-3][i])
-				       + hc[4]*(vy[j+3][i]-vy[j-4][i])
-				       + hc[5]*(vy[j+4][i]-vy[j-5][i])
-				      )*dhi;
-				vyx = (  hc[1]*(vy[j][i+1]-vy[j][i])
+				       + hc[5]*(vx[j][i+4]-vx[j][i-5]))*dhi;
+			
+			vyx = (  hc[1]*(vy[j][i+1]-vy[j][i])
 				       + hc[2]*(vy[j][i+2]-vy[j][i-1])
 				       + hc[3]*(vy[j][i+3]-vy[j][i-2])
 				       + hc[4]*(vy[j][i+4]-vy[j][i-3])
-				       + hc[5]*(vy[j][i+5]-vy[j][i-4])
-				      )*dhi;
-				vxy = (  hc[1]*(vx[j+1][i]-vx[j][i])
+				       + hc[5]*(vy[j][i+5]-vy[j][i-4]))*dhi;
+
+                        vxy = (  hc[1]*(vx[j+1][i]-vx[j][i])
 				       + hc[2]*(vx[j+2][i]-vx[j-1][i])
 				       + hc[3]*(vx[j+3][i]-vx[j-2][i])
 				       + hc[4]*(vx[j+4][i]-vx[j-3][i])
-				       + hc[5]*(vx[j+5][i]-vx[j-4][i])
-				      )*dhi;
-				      
-				ux[j][i] += DT*vxx;
-				uy[j][i] += DT*vyy;      
-		       		
-				fipjp=uipjp[j][i]*DT;
-				f=u[j][i]*DT;
-				g=pi[j][i]*DT;
-				
-				sxy[j][i]+=(fipjp*(vxy+vyx));
-				sxx[j][i]+=(g*(vxx+vyy))-(2.0*f*vyy);
-				syy[j][i]+=(g*(vxx+vyy))-(2.0*f*vxx);*/
-				
-				/*
-				piv = pi[j][i]*(vxx+vyy);
-				sxy[j][i]+=(uipjp[j][i]*(vxy+vyx));
-				sxx[j][i]+=piv-(2.0*u[j][i]*vyy);
-				syy[j][i]+=piv-(2.0*u[j][i]*vxx);
-				*/
-/*			}
+				       + hc[5]*(vx[j+5][i]-vx[j-4][i]))*dhi;
+
+                        vyy = (  hc[1]*(vy[j][i]  -vy[j-1][i])
+				       + hc[2]*(vy[j+1][i]-vy[j-2][i])
+				       + hc[3]*(vy[j+2][i]-vy[j-3][i])
+				       + hc[4]*(vy[j+3][i]-vy[j-4][i])
+				       + hc[5]*(vy[j+4][i]-vy[j-5][i]))*dhi; 
+
+        /* left boundary */                                         
+        if((!BOUNDARY) && (POS[1]==0) && (i<=FW)){
+                        
+                        psi_vxx[j][i] = b_x[i] * psi_vxx[j][i] + a_x[i] * vxx;
+                        vxx = vxx / K_x[i] + psi_vxx[j][i];
+
+                        psi_vyx[j][i] = b_x_half[i] * psi_vyx[j][i] + a_x_half[i] * vyx;
+                        vyx = vyx / K_x_half[i] + psi_vyx[j][i];                 
+         }
+
+        /* right boundary */                                         
+        if((!BOUNDARY) && (POS[1]==NPROCX-1) && (i>=nx2-FW+1)){
+		
+                        h1 = (i-nx2+2*FW);
+                        h = i;
+                        
+                        psi_vxx[j][h1] = b_x[h1] * psi_vxx[j][h1] + a_x[h1] * vxx;
+                        vxx = vxx / K_x[h1] + psi_vxx[j][h1]; 
+
+                        /*psi_vyx[j][h] = b_x_half[h] * psi_vyx[j][h] + a_x_half[h] * vyx;
+                        vyx = vyx / K_x_half[h] + psi_vyx[j][h];*/
+                        
+                        psi_vyx[j][h1] = b_x_half[h1] * psi_vyx[j][h1] + a_x_half[h1] * vyx;
+			vyx = vyx / K_x_half[h1] + psi_vyx[j][h1];
+                                           
+         }
+
+	  /* top boundary */                                         
+        if((POS[2]==0) && (!(FREE_SURF)) && (j<=FW)){
+                                                
+                        psi_vyy[j][i] = b_y[j] * psi_vyy[j][i] + a_y[j] * vyy;                                            
+                        psi_vxy[j][i] = b_y_half[j] * psi_vxy[j][i] + a_y_half[j] * vxy;
+                     
+                        vyy = vyy / K_y[j] + psi_vyy[j][i];
+                        vxy = vxy / K_y_half[j] + psi_vxy[j][i];
+
+        }
+	
+	  /* bottom boundary */                                         
+        if((POS[2]==NPROCY-1) && (j>=ny2-FW+1)){
+
+                        h1 = (j-ny2+2*FW);                                        
+                        h = j;
+                                                
+                        psi_vyy[h1][i] = b_y[h1] * psi_vyy[h1][i] + a_y[h1] * vyy;                                            
+                        vyy = vyy / K_y[h1] + psi_vyy[h1][i];
+                        
+                        /*psi_vxy[j][i] = b_y_half[j] * psi_vxy[j][i] + a_y_half[j] * vxy;
+                        vxy = vxy / K_y_half[j] + psi_vxy[j][i];*/
+                        
+                        psi_vxy[h1][i] = b_y_half[h1] * psi_vxy[h1][i] + a_y_half[h1] * vxy;
+			vxy = vxy / K_y_half[h1] + psi_vxy[h1][i];
+        
+        }
+
+	/* computing sums of the old memory variables */
+			sumr=sump=sumq=0.0;
+			for (l=1;l<=L;l++){
+				sumr+=r[j][i][l];
+				sump+=p[j][i][l];
+				sumq+=q[j][i][l];
+			}
+
+
+                        /* updating components of the stress tensor, partially */
+			sxy[j][i] += (fipjp[j][i]*(vxy+vyx))+(dthalbe*sumr);
+			sxx[j][i] += (g[j][i]*(vxx+vyy))-(2.0*f[j][i]*vyy)+(dthalbe*sump);
+			syy[j][i] += (g[j][i]*(vxx+vyy))-(2.0*f[j][i]*vxx)+(dthalbe*sumq);
+			
+			
+			/* now updating the memory-variables and sum them up*/
+			sumr=sump=sumq=0.0;
+			for (l=1;l<=L;l++){
+				r[j][i][l] = bip[l]*(r[j][i][l]*cip[l]-(dip[j][i][l]*(vxy+vyx)));
+				p[j][i][l] = bjm[l]*(p[j][i][l]*cjm[l]-(e[j][i][l]*(vxx+vyy))+(2.0*d[j][i][l]*vyy));
+				q[j][i][l] = bjm[l]*(q[j][i][l]*cjm[l]-(e[j][i][l]*(vxx+vyy))+(2.0*d[j][i][l]*vxx));
+				sumr += r[j][i][l];
+				sump += p[j][i][l];
+				sumq += q[j][i][l];
+			}
+
+			/* and now the components of the stress tensor are
+			   completely updated */
+			sxy[j][i]+=(dthalbe*sumr);
+			sxx[j][i]+=(dthalbe*sump);
+			syy[j][i]+=(dthalbe*sumq);
+			
+			/* save forward wavefield for gradient calculation */
+			if((INVMAT==0)&&(GRAD_FORM==2)){
+			  ux[j][i] = sump;
+		          uy[j][i] = sumq;
+			  uxy[j][i] = sumr;
+			}
+			
+			if((INVMAT==0)&&(GRAD_FORM==3)){
+			  ux[j][i] = vxx;
+		          uy[j][i] = vyy;
+			  uxy[j][i] = vxy+vyx;
+			}
+			
+
+			}
 		}
 		break;
 		
 	case 12:
 		for (j=ny1;j<=ny2;j++){
 			for (i=nx1;i<=nx2;i++){
-				vxx = (  hc[1]*(vx[j][i]  -vx[j][i-1])
+			
+			vxx = (  hc[1]*(vx[j][i]  -vx[j][i-1])
 				       + hc[2]*(vx[j][i+1]-vx[j][i-2])
 				       + hc[3]*(vx[j][i+2]-vx[j][i-3])
 				       + hc[4]*(vx[j][i+3]-vx[j][i-4])
 				       + hc[5]*(vx[j][i+4]-vx[j][i-5])
-				       + hc[6]*(vx[j][i+5]-vx[j][i-6])
-				      )*dhi;
-				vyy = (  hc[1]*(vy[j][i]  -vy[j-1][i])
-				       + hc[2]*(vy[j+1][i]-vy[j-2][i])
-				       + hc[3]*(vy[j+2][i]-vy[j-3][i])
-				       + hc[4]*(vy[j+3][i]-vy[j-4][i])
-				       + hc[5]*(vy[j+4][i]-vy[j-5][i])
-				       + hc[6]*(vy[j+5][i]-vy[j-6][i])
-				      )*dhi;
-				vyx = (  hc[1]*(vy[j][i+1]-vy[j][i])
+				       + hc[6]*(vx[j][i+5]-vx[j][i-6]))*dhi;
+			
+			vyx = (  hc[1]*(vy[j][i+1]-vy[j][i])
 				       + hc[2]*(vy[j][i+2]-vy[j][i-1])
 				       + hc[3]*(vy[j][i+3]-vy[j][i-2])
 				       + hc[4]*(vy[j][i+4]-vy[j][i-3])
 				       + hc[5]*(vy[j][i+5]-vy[j][i-4])
-				       + hc[6]*(vy[j][i+6]-vy[j][i-5])
-				      )*dhi;
-				vxy = (  hc[1]*(vx[j+1][i]-vx[j][i])
+				       + hc[6]*(vy[j][i+6]-vy[j][i-5]))*dhi;
+
+                        vxy = (  hc[1]*(vx[j+1][i]-vx[j][i])
 				       + hc[2]*(vx[j+2][i]-vx[j-1][i])
 				       + hc[3]*(vx[j+3][i]-vx[j-2][i])
 				       + hc[4]*(vx[j+4][i]-vx[j-3][i])
 				       + hc[5]*(vx[j+5][i]-vx[j-4][i])
-				       + hc[6]*(vx[j+6][i]-vx[j-5][i])
-				      )*dhi;
-				      
-				ux[j][i] += DT*vxx;
-				uy[j][i] += DT*vyy;
-		       
-				fipjp=uipjp[j][i]*DT;
-				f=u[j][i]*DT;
-				g=pi[j][i]*DT;
+				       + hc[6]*(vx[j+6][i]-vx[j-5][i]))*dhi;
 
-				sxy[j][i]+=(fipjp*(vxy+vyx));
-				sxx[j][i]+=(g*(vxx+vyy))-(2.0*f*vyy);
-				syy[j][i]+=(g*(vxx+vyy))-(2.0*f*vxx);
+                        vyy = (  hc[1]*(vy[j][i]  -vy[j-1][i])
+				       + hc[2]*(vy[j+1][i]-vy[j-2][i])
+				       + hc[3]*(vy[j+2][i]-vy[j-3][i])
+				       + hc[4]*(vy[j+3][i]-vy[j-4][i])
+				       + hc[5]*(vy[j+4][i]-vy[j-5][i])
+				       + hc[6]*(vy[j+5][i]-vy[j-6][i]))*dhi; 
+
+        /* left boundary */                                         
+        if((!BOUNDARY) && (POS[1]==0) && (i<=FW)){
+                        
+                        psi_vxx[j][i] = b_x[i] * psi_vxx[j][i] + a_x[i] * vxx;
+                        vxx = vxx / K_x[i] + psi_vxx[j][i];
+
+                        psi_vyx[j][i] = b_x_half[i] * psi_vyx[j][i] + a_x_half[i] * vyx;
+                        vyx = vyx / K_x_half[i] + psi_vyx[j][i];                 
+         }
+
+        /* right boundary */                                         
+        if((!BOUNDARY) && (POS[1]==NPROCX-1) && (i>=nx2-FW+1)){
+		
+                        h1 = (i-nx2+2*FW);
+                        h = i;
+                        
+                        psi_vxx[j][h1] = b_x[h1] * psi_vxx[j][h1] + a_x[h1] * vxx;
+                        vxx = vxx / K_x[h1] + psi_vxx[j][h1]; 
+
+                        /*psi_vyx[j][h] = b_x_half[h] * psi_vyx[j][h] + a_x_half[h] * vyx;
+                        vyx = vyx / K_x_half[h] + psi_vyx[j][h];*/
+                        
+                        psi_vyx[j][h1] = b_x_half[h1] * psi_vyx[j][h1] + a_x_half[h1] * vyx;
+			vyx = vyx / K_x_half[h1] + psi_vyx[j][h1];
+                                           
+         }
+
+	  /* top boundary */                                         
+        if((POS[2]==0) && (!(FREE_SURF)) && (j<=FW)){
+                                                
+                        psi_vyy[j][i] = b_y[j] * psi_vyy[j][i] + a_y[j] * vyy;                                            
+                        psi_vxy[j][i] = b_y_half[j] * psi_vxy[j][i] + a_y_half[j] * vxy;
+                     
+                        vyy = vyy / K_y[j] + psi_vyy[j][i];
+                        vxy = vxy / K_y_half[j] + psi_vxy[j][i];
+
+        }
+	
+	  /* bottom boundary */                                         
+        if((POS[2]==NPROCY-1) && (j>=ny2-FW+1)){
+
+                        h1 = (j-ny2+2*FW);                                        
+                        h = j;
+                                                
+                        psi_vyy[h1][i] = b_y[h1] * psi_vyy[h1][i] + a_y[h1] * vyy;                                            
+                        vyy = vyy / K_y[h1] + psi_vyy[h1][i];
+                        
+                        /*psi_vxy[j][i] = b_y_half[j] * psi_vxy[j][i] + a_y_half[j] * vxy;
+                        vxy = vxy / K_y_half[j] + psi_vxy[j][i];*/
+                        
+                        psi_vxy[h1][i] = b_y_half[h1] * psi_vxy[h1][i] + a_y_half[h1] * vxy;
+			vxy = vxy / K_y_half[h1] + psi_vxy[h1][i];
+        
+        }
+
+	                /* computing sums of the old memory variables */
+			sumr=sump=sumq=0.0;
+			for (l=1;l<=L;l++){
+				sumr+=r[j][i][l];
+				sump+=p[j][i][l];
+				sumq+=q[j][i][l];
+			}
+
+
+                        /* updating components of the stress tensor, partially */
+			sxy[j][i] += (fipjp[j][i]*(vxy+vyx))+(dthalbe*sumr);
+			sxx[j][i] += (g[j][i]*(vxx+vyy))-(2.0*f[j][i]*vyy)+(dthalbe*sump);
+			syy[j][i] += (g[j][i]*(vxx+vyy))-(2.0*f[j][i]*vxx)+(dthalbe*sumq);
+			
+			
+			/* now updating the memory-variables and sum them up*/
+			sumr=sump=sumq=0.0;
+			for (l=1;l<=L;l++){
+				r[j][i][l] = bip[l]*(r[j][i][l]*cip[l]-(dip[j][i][l]*(vxy+vyx)));
+				p[j][i][l] = bjm[l]*(p[j][i][l]*cjm[l]-(e[j][i][l]*(vxx+vyy))+(2.0*d[j][i][l]*vyy));
+				q[j][i][l] = bjm[l]*(q[j][i][l]*cjm[l]-(e[j][i][l]*(vxx+vyy))+(2.0*d[j][i][l]*vxx));
+				sumr += r[j][i][l];
+				sump += p[j][i][l];
+				sumq += q[j][i][l];
+			}
+
+			/* and now the components of the stress tensor are
+			   completely updated */
+			sxy[j][i]+=(dthalbe*sumr);
+			sxx[j][i]+=(dthalbe*sump);
+			syy[j][i]+=(dthalbe*sumq);
+			
+			/* save forward wavefield for gradient calculation */
+			if((INVMAT==0)&&(GRAD_FORM==2)){
+			  ux[j][i] = sump;
+		          uy[j][i] = sumq;
+			  uxy[j][i] = sumr;
+			}
+			
+			if((INVMAT==0)&&(GRAD_FORM==3)){
+			  ux[j][i] = vxx;
+		          uy[j][i] = vyy;
+			  uxy[j][i] = vxy+vyx;
+			}
+
+		
 			}
 		}
 		break;
-*/
+
 		
 	default:
 		for (j=ny1;j<=ny2;j++){
